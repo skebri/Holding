e7d482c89a4815926f2e8ed81d97d0618354d761
skebri <brisevacstefan@gmail.com>
2017-08-01 13:30:07 +0200

    add all

---
+++ main.js
@@ -0,0 +1,27 @@
+setTimeout(function () {
+  $('.section').toggleClass('visible');
+}, 100);
+
+//============================================================Animation2
+
+$('[has-ripple="true"]').click(function () {
+  $(this).toggleClass('clicked');
+  $('.menu').toggleClass('open');
+});
+
+$('.menu a').each(function (index) {
+  var thismenuItem        = $(this);
+  
+  thismenuItem.click(function (event) {
+    event.preventDefault();
+    
+    $('.menuitem-wrapper').eq(index).addClass('spin');
+    
+    var timer = setTimeout(function () {
+      $('body').removeAttr('class').addClass('bg-'+index);
+      $('.menuitem-wrapper').eq(index).removeClass('spin');
+      $('.menu').removeClass('open');
+      $('.menu-btn').removeClass('clicked');
+    }, 800);
+  });
+});
